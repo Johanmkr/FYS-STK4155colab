Tried fixing scaling problem, still stuck. Experimenting in test.py

project1/code/main.py
@@ -10,21 +10,11 @@ PLOT.init('off')
 
 
 
-testSize = 1/5
 
 Nx, Ny = 20, 20
 x = np.linspace(0, 1, Nx)
 y = np.linspace(0, 1, Ny)
 x, y = np.meshgrid(x, y)
-
-
-def FrankeFunction(x, y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
-
 
 noise = lambda eta: eta*np.random.randn(Ny, Nx)
 
@@ -46,17 +36,16 @@ def ptB():
         dM.createX(x, y)
             
         reg = LinearRegression(z, dM)
-        #reg.scale()
+        #r
         trainer, predictor = reg.split()
-        #trainer.scale()
-        #predictor.scale()
+        #reg.scale()
         beta = trainer.train() 
-        trainer.fit()
+        trainer.computeModel()
         
         trainer.computeExpectationValues()
 
         predictor.setOptimalbeta(beta)
-        predictor.fit()
+        predictor.computeModel()
         predictor.computeExpectationValues()
 
         Trainings.append(trainer)
@@ -65,7 +54,7 @@ def ptB():
         if n == main_polydeg:
             beta = reg()
             reg.setOptimalbeta(beta)
-            reg.fit()
+            reg.computeModel()
             REG5 = reg
 
     PLOT.ptB_franke_funcion(x, y, REG5, show=True)
@@ -91,11 +80,11 @@ def ptC():
         #trainer.scale()
         #predictor.scale()
         beta = trainer.train() 
-        trainer.fit()
+        trainer.computeModel()
         trainer.computeExpectationValues()
 
         predictor.setOptimalbeta(beta)
-        predictor.fit()
+        predictor.computeModel()
         predictor.computeExpectationValues()
 
         Trainings.append(trainer)
@@ -116,9 +105,45 @@ def ptC():
 
 
 
+def ptE():
+    # ....
+    polydegs = range(1,20+1)
+
+    Trainings = []
+    Predictions = [] 
+
+    for n in polydegs:
+        dM = DesignMatrix(n)
+        dM.createX(x, y)
+        reg = LinearRegression(z, dM, 'Ridge')
+
+        trainer, predictor = reg.split()
+        beta = trainer.train(0.1) 
+        trainer.computeModel()
+        trainer.computeExpectationValues()
+
+        predictor.setOptimalbeta(beta)
+        predictor.computeModel()
+        predictor.computeExpectationValues()
+
+        Trainings.append(trainer)
+        Predictions.append(predictor)
 
 
-all_pts = ['B', 'C']
+    Bootstrappings = []
+    for train, test in zip(Trainings, Predictions):
+        BS = Bootstrap(train, test)
+        BS()
+        BS.bias_varianceDecomposition()
+        Bootstrappings.append(BS)
+
+
+
+
+
+
+
+all_pts = ['B', 'C', 'E']
 
 def runparts(parts):
     pts = []

project1/code/src/utils.py
@@ -10,6 +10,20 @@ np.random.seed(ourSeed)
 randomState = check_random_state(ourSeed)
 
 
+
+
+
+def FrankeFunction(x, y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
+
+
+
+
+
 def polydeg2features(polydeg):
     """
     Function for finding the length of β for a given polynomial degree in 2 dimensions.
@@ -27,9 +41,6 @@ def polydeg2features(polydeg):
     n = polydeg
     p = int((n+1)*(n+2)/2)
     return p
-
-
-
 
 
 def features2polydeg(features):

project1/code/test.py
@@ -1,148 +1,65 @@
-import numpy as np
-import sys
-import os
-import pandas as pd
-
-
-print('\nImporting...\n')
-# sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+from src.utils import *
 
 from src.designMatrix import DesignMatrix
-from src.Regression import LeastSquares
+from src.Regression import LinearRegression
 from src.Resampling import Bootstrap
+from src.parameterVector import ParameterVector
 
-from src.betaMatrix import betaMatrix
 
-testSize = 1/5
 
-Nx, Ny = 60, 60
-x = np.linspace(0, 1, Nx)
-y = np.linspace(0, 1, Ny)
+
+Nx, Ny = 20, 20
+x = np.random.rand(Nx)
+y = np.random.rand(Ny)
 x, y = np.meshgrid(x, y)
 
 
-print('\ndesign matrix\n')
+noise = lambda eta: eta*np.random.randn(Ny, Nx)
+eta = 0.01
+z = FrankeFunction(x, y) + noise(eta)
+
+
 dM = DesignMatrix(5)
 
-print('Creating X\n')
 dM.createX(x, y)
+reg = LinearRegression(z, dM, mode='own')
+trainer, predictor = reg.split()
 
+# train:
+beta = trainer.train()
+trainer.computeModel()
+trainer.computeExpectationValues()
 
-def FrankeFunction(x, y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
+# predict:
+predictor.setOptimalbeta(beta)
+ztildeown = predictor.computeModel()
+predictor.computeExpectationValues()
+print(beta)
+print(predictor.MSE)
 
-
-noise = lambda eta: eta*np.random.randn(Ny, Nx)
-z = FrankeFunction(x, y) + noise(0.1)
-
-print('\nregression...\n')
-reg = LeastSquares(z, dM, mode='skl')
-
+### with scaling
 reg.scale()
 
-trainer, predictor = reg.split()
-trainer.train()
+# train:
+beta = trainer.train()
+trainer.computeModel()
+trainer.computeExpectationValues()
+
+# predict:
+predictor.setOptimalbeta(beta)
+ztildeskl = predictor.computeModel()
+predictor.computeExpectationValues()
+print(beta)
+print(predictor.MSE)
 
 
-
-BS = Bootstrap(trainer, predictor)
-BS(10)
-#print(BS.betas)
-#print(BS.betas2)
-
-BS.bias_varianceDecomposition()
-
-
-
-
-sys.exit()
-
-
-# DIVERSE:
-
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-
-
-
-from sklearn.model_selection import train_test_split
-
-
-X_train, X_test, z_train, z_test = train_test_split(dM.X, z.ravel(), test_size=testSize)
-
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
-
-dM_train = dM.newObject(X_train)
-dM_test = dM.newObject(X_test)
-
-def OrdinaryLeastSquares(designMatrix, z_data):
-    X = designMatrix.X
-    Hinv = designMatrix.Hinv
-    beta = Hinv @ X.T @ z_data.ravel()
-    return beta
-
-beta = OrdinaryLeastSquares(dM_train, z_train)
-betapd = pd.DataFrame(data=beta, index=[f'β_{j}' for j in range(len(beta))])
+for zs, zo in zip(ztildeown, ztildeskl):
+    print(zs, zo)
 
 
 
 
-def mean_squared_error(data, model):
-    MSE = np.sum((data.ravel()-model.ravel())**2)/np.size(data.ravel())
-    return MSE
 
-def bias_squared(data, model):
-    #data = data.ravel()
-    #model = model.ravel()
-    #bias2 = np.dot((data - np.mean(model)), (data - np.mean(model)))
-    bias2 = mean_squared_error(data, np.mean(model))
-    return bias2
-
-def variance(model):
-    var = np.var(model)
-    return var
-
-
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-
-
-
-polydegs = [1,2,3,4,5,6]
-beta_list = []
-MSE_list = []
-bias2_list = []
-var_list = []
-
-for n in polydegs:
-    dM = DesignMatrix(n)
-    dM.create_X(x, y)
-    X_train, X_test, z_train, z_test = train_test_split(dM.X, z.ravel(), test_size=testSize)
-
-    dM_train = dM.newObject(X_train)
-    dM_test = dM.newObject(X_test)
-    beta = OrdinaryLeastSquares(dM_train, z_train)
-
-    ztilde = dM_train.X @ beta
-    zpredict = dM_test.X @ beta
-
-    MSE = mean_squared_error(zpredict, z_test)
-    bias2 = bias_squared(zpredict, z_test)
-    var = variance(zpredict)
-
-    beta_list.append(beta)
-    MSE_list.append(MSE)
-    bias2_list.append(bias2)
-    var_list.append(var)
-    print(f'\nn = {n}')
-    print('     MSE: ', MSE)
-    print('  bias^2: ', bias2)
-    print('variance: ', var)
 
 
 