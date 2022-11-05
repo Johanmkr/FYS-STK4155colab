a1bbcea3ec6e971171fbe8b69086478e71149944
johanmkr <johan.m.kroken@gmail.com>
2022-11-05 14:57:03 +0100

    starting to sort out the dimensionalytu stuff

--- project2/code/src/devNeuralNetwork.py
+++ project2/code/src/devNeuralNetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Layer import Layer
 from LossFunctions import LossFunctions
-from WeightMatrix import WeightMatrix
+# from WeightMatrix import WeightMatrix
 from ActivationFunction import ActivationFunction
 import GradientDescent as GD
 from DataHandler import DataHandler
@@ -48,7 +48,7 @@ class devNeuralNetwork:
         self.setInputFeatures(data)
         self.layers[0].UpdateLayer(self.inputs, self.features)
         if data.ndim == 1 or data.ndim:
-            self.layers[0].h[:,0] = data
+            self.layers[0].h = data
         else:
             self.layers[0].h = data
         # print(self.layers[0].h[:,0])
@@ -105,16 +105,20 @@ class devNeuralNetwork:
 
 
     def initialiseWeights(self):
-        self.weights = []
-        for i in range(self.nrOfLayers-1):
-            nIn = self.layers[i].neurons
-            nOut = self.layers[i+1].neurons
-            self.weights.append(WeightMatrix(nIn, nOut))
+        # self.weights = []
+        for i in range(1, self.nrOfLayers): #looping over hidden layers + output layer
+            nIn = self.layers[i-1].neurons
+            nOut = self.layers[i].neurons
+            # self.weights.append(WeightMatrix(nIn, nOut))
+            self.layers[i].setWmatrix(nIn,nOut)
 
     def feedForward(self, train=False):
         for i in range(1, self.nrOfLayers):
             # embed()
-            w = self.weights[i-1].w 
+
+            # w = self.weights[i-1].w 
+
+            w = self.layers[i].w
             h = self.layers[i-1].h
             b = self.layers[i].bias
             # activator = self.weights[i-1].w.T @ self.layers[i-1].h + self.layers[i].bias
@@ -133,18 +137,38 @@ class devNeuralNetwork:
         
     def backPropagation(self):
         eta = 0.001
-        self.layers[-1].delta = self.activationFunction.derivative(self.layers[-1].a) * self.lossFunction.derivative(self.outputData, self.targetData[:,np.newaxis])
-
+        self.layers[-1].delta = self.activationFunction.derivative(self.layers[-1].a) * self.lossFunction.derivative(self.outputData, self.targetData)
         #   Find deltas
         for l in range(self.nrOfLayers-2, 0, -1):
+            delta = self.layers[l+1].delta
 
-            self.layers[l].delta =  self.layers[l+1].delta @ self.weights[l].w * self.activationFunction.derivative(self.layers[l].a)
-        # embed()
+            # w = self.weights[l].w
+            
+            w = self.layers[l+1].w
+            a = self.layers[l].a
 
-        for l in range(self.nrOfLayers-2,-1,-1):
-            self.weights[l].w = self.weights[l].w - eta * self.layers[l+1].delta.T @ self.layers[l].h
+            self.layers[l].delta = delta @ w * self.activationFunction.derivative(a)
+
+            # self.layers[l].delta =  self.layers[l+1].delta @ self.weights[l].w * self.activationFunction.derivative(self.layers[l].a)
+
+
+
+        # for l in range(self.nrOfLayers-2,-1,-1):
+        #     self.weights[l].w = self.weights[l].w - eta * self.layers[l+1].delta.T @ self.layers[l].h
+        # for l in range(self.nrOfLayers-1, 0, -1):
+        #     self.layers[l].bias = self.layers[l].bias -eta * np.sum(self.layers[l].delta, axis=0)
+
+
+        # Testing new approach
         for l in range(self.nrOfLayers-1, 0, -1):
-            self.layers[l].bias = self.layers[l].bias -eta * np.sum(self.layers[l].delta, axis=0)
+            w = self.layers[l].w
+            delta = self.layers[l].delta 
+            h = self.layers[l-1].h
+            bias = self.layers[l].bias 
+
+            self.layers[l].w = w - eta * delta.T @ h 
+            self.layers[l].bias = bias - eta * np.sum(delta, axis=0)
+
         # self.weights[0].w = self.weights[0].w - eta * self.layers[1].delta.T @ self.layers[0].h
     
             #this must be tied together with the gradient descent code.
@@ -160,23 +184,30 @@ if __name__=="__main__":
     x = np.linspace(-5,5, 100)
     y = x*np.cos(x) + 0.5*np.random.randn(len(x))
     ynorm = (y-np.mean(y))/np.std(y)
+    x = x[:,np.newaxis]
+    ynorm = ynorm[:,np.newaxis]
     plt.plot(x,ynorm, ".", label="data")
     # plt.show()
-    dummy = devNeuralNetwork(x, ynorm, hiddenLayers=3, neuronsInEachLayer=5)
+    dummy = devNeuralNetwork(x, ynorm, hiddenLayers=3, neuronsInEachLayer=4)
     print(dummy)
     # dummy.train()
     # dummy()
-    Niter = 15
+    Niter = 16
+    dummy.train()
+    pred = dummy()
+    plt.plot(x, pred, label="Untrained")
     LF = LossFunctions()
-    for i in range(Niter):
-        dummy.train(1000)
+    for i in range(1, Niter+1):
+        N =  (250*i)
+        dummy.train(N)
         pred = dummy()
-        #print(LF(pred, y))
+        # embed()
         loss = np.mean(LF(pred, ynorm))
         # embed()
-        print(f"Loss for N = {i+1}: {loss:.2f}")
+        print(f"Loss for N = {i}: {loss:.2f}  with {N} iterations")
+        if i % 4 == 0:
+            plt.plot(x,pred, label=f"N={i}")
     
-    plt.plot(x,pred, label=f"N={i+1}")
 
     # xtest = np.linspace(-10,10,1000)
 
